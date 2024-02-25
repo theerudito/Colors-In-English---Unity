16093106f52838c7a88e11e7f4dd0a7f118ed568
theerudito <erudito.tv@gmail.com>
2024-02-25 16:31:37 -0500

    v2

--- Assets/scripts/Game/GameManager.cs
+++ Assets/scripts/Game/GameManager.cs
@@ -8,6 +8,7 @@ using Dapper;
 using System.Threading.Tasks;
 using System.Collections.Generic;
 using System.Linq;
+using System.Collections;
 
 public class GameManager : MonoBehaviour
 {
@@ -15,7 +16,9 @@ public class GameManager : MonoBehaviour
     private string _developer = "MADE WITH BY BETWEEN BYTE SOFTWARE " + "- " + DateTime.Now.Year.ToString();
     private int _score = 0;
     private int _points = 10;
-    private int _time = 60;
+    private int _time = 30;
+    private float _updateInterval = 1f;
+    private float _nextUpdateTime;
 
 
     [SerializeField] private TextMeshProUGUI _labelDeveloper;
@@ -28,6 +31,10 @@ public class GameManager : MonoBehaviour
     [SerializeField] private TextMeshProUGUI _labelColor;
     [SerializeField] private Button[] _buttonsColors;
     [SerializeField] private TextMeshProUGUI[] _textInButtons;
+    [SerializeField] private AudioClip _soundCorrect;
+    [SerializeField] private AudioClip _soundIncorrect;
+    [SerializeField] private AudioClip _soundClock;
+    [SerializeField] private AudioSource _audioSource;
 
     void Start()
     {
@@ -36,12 +43,13 @@ public class GameManager : MonoBehaviour
         _labelDeveloper.text = _developer;
         _labelPath = GameObject.Find("labelPath").GetComponent<TextMeshProUGUI>();
         _labelScore = GameObject.Find("score").GetComponent<TextMeshProUGUI>();
-        _labelTime = GameObject.Find("labelTime").GetComponent<TextMeshProUGUI>();
+        _labelTime = GameObject.Find("time").GetComponent<TextMeshProUGUI>();
         _imageHappy = GameObject.Find("imgHappy").GetComponent<Image>();
         _imageSad = GameObject.Find("imgSad").GetComponent<Image>();
         _imageDefault = GameObject.Find("imgDefault").GetComponent<Image>();
         _labelColor = GameObject.Find("labelColor").GetComponent<TextMeshProUGUI>();
-
+        _audioSource = GameObject.Find("Canvas Game").GetComponent<AudioSource>();
+        StartCoroutine(CountdownTimer());
         GenerateColors();
     }
 
@@ -93,15 +101,24 @@ public class GameManager : MonoBehaviour
             _imageHappy.gameObject.SetActive(true);
             _imageSad.gameObject.SetActive(false);
             _imageDefault.gameObject.SetActive(false);
-            ResetData();
+            _audioSource.clip = _soundCorrect;
+            _audioSource.Play();
+            StartCoroutine(CountdownTimer());
+            StartCoroutine(ResetData());
         }
         else
         {
-            _score -= _points;
+            if (_score == 0)
+                _score = 10;
+            else
+                _score -= _points;
             _labelScore.text = _score.ToString();
             _imageHappy.gameObject.SetActive(false);
             _imageSad.gameObject.SetActive(true);
             _imageDefault.gameObject.SetActive(false);
+            _audioSource.clip = _soundIncorrect;
+            _audioSource.Play();
+
         }
     }
 
@@ -141,12 +158,48 @@ public class GameManager : MonoBehaviour
         return color;
     }
 
-    private void ResetData()
+    private IEnumerator ResetData()
     {
-        Task.Delay(2000);
+        _buttonsColors.ToList().ForEach(btn => btn.interactable = false);
+        yield return new WaitForSeconds(1f);
+        _buttonsColors.ToList().ForEach(btn => btn.interactable = true);
         _imageHappy.gameObject.SetActive(false);
         _imageSad.gameObject.SetActive(false);
         _imageDefault.gameObject.SetActive(true);
         GenerateColors();
+        _labelTime.color = Color.white;
+        _time = 30;
+        _labelTime.text = _time.ToString();
+        StartCoroutine(CountdownTimer());
+    }
+
+    private IEnumerator CountdownTimer()
+    {
+        _nextUpdateTime = Time.time + _updateInterval;
+
+        while (_time > 0)
+        {
+            yield return null;
+
+            if (Time.time >= _nextUpdateTime)
+            {
+                _time--;
+                _labelTime.text = _time.ToString();
+
+                if (_time == 15)
+                {
+                    _labelTime.color = Color.red;
+                    _audioSource.clip = _soundClock;
+                    _audioSource.Play();
+                }
+
+                _nextUpdateTime += _updateInterval;
+            }
+        }
+        StartCoroutine(ResetData());
+    }
+    public void LauncherURL(string url)
+    {
+        Application.OpenURL(url);
     }
 }
